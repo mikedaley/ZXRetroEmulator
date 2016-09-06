77caf8354e903a7b567891b03ce6bc711e66a2de
Mike Daley <mike@71squared.com>
2016-09-06 16:32:37 +0100

    Added interrupt timing tstate check

--- ZXRetroEmulator/Z80Core.cpp
+++ ZXRetroEmulator/Z80Core.cpp
@@ -148,7 +148,7 @@ int CZ80Core::Execute(int num_tstates)
 		// First process an interrupt
         if (m_CPURegisters.IntReq)
         {
-            if (m_CPURegisters.EIHandled == false && m_CPURegisters.IFF1 != 0 )
+            if (m_CPURegisters.IntReq && m_CPURegisters.EIHandled == false && m_CPURegisters.IFF1 != 0 )
             {
                 // First see if we are halted?
                 if ( m_CPURegisters.Halted )
